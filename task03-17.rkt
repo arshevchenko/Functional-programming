6b0c597943e979abb344c1948b4b4c8a1c203788
Artem Shevchenko <shevchenkoaa@sgu.ru>
2016-09-23 11:45:40 +0300

    Rewrited Task#3

--- task03-17.rkt
+++ task03-17.rkt
@@ -1,14 +1,15 @@
 #lang racket
-(cons
- (cons
-       (cons 'A
-             (cons 'B
-                   (cons 'C
-                         (cons 'D
-                                  (cons 'E '())))))
-       (cons
-         (cons'F
-             (cons
-              (cons 'X
-                    (cons
-                     (cons 'G '()) '())) 'H)) '()))'())
+(let ((1st (cons 'A
+                 (cons 'B
+                       (cons 'C
+                             (cons 'D
+                                   (cons 'E '()))))))
+      (2nd (cons 'F
+                 (cons
+                  (cons 'X
+                        (cons
+                         (cons 'G '()) '())) '()))))
+   (cons
+    (cons 1st
+          (cons 2nd
+                (cons 'H '()))) '()))
